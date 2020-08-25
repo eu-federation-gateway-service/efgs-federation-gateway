6dd762ce8ccfc1669a9dc149a8003bd6d2845cc9
Felix Dittrich <felix.dittrich@t-systems.com>
2020-08-25 14:36:31 +0200

    Optimized Imports

--- src/main/java/eu/interop/federationgateway/controller/CallbackAdminController.java
+++ src/main/java/eu/interop/federationgateway/controller/CallbackAdminController.java
@@ -45,7 +45,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
