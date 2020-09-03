b4583eef257267647fafe40a9dd38e8249f22892
Maximilian Laue <65015235+mlaue-tech@users.noreply.github.com>
2020-09-03 18:36:41 +0200

    Feat: Input-Validator for DiagnosisKeys (#95)
    * feat: input validator for all diagnosis keys + exception handler for ConstraintValidationException
    
    * fix: Test-failure
    
    * feat: Added Junit Tests for Input Validator
    
    * fix: Javadoc for new Errorhandling method
    
    * fix: clean code
    
    * fix: typos

---
+++ src/test/java/eu/interop/federationgateway/validator/DiagnosisKeyBatchValidatorTest.java
@@ -0,0 +1,161 @@
+package eu.interop.federationgateway.validator;
+
+import com.google.protobuf.ByteString;
+import eu.interop.federationgateway.TestData;
+import eu.interop.federationgateway.batchsigning.SignatureGenerator;
+import eu.interop.federationgateway.config.EfgsProperties;
+import eu.interop.federationgateway.filter.CertificateAuthentificationFilter;
+import eu.interop.federationgateway.model.EfgsProto;
+import eu.interop.federationgateway.repository.CertificateRepository;
+import eu.interop.federationgateway.repository.DiagnosisKeyEntityRepository;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.bouncycastle.cert.CertIOException;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DiagnosisKeyBatchValidatorTest {
+
+  @Autowired
+  private WebApplicationContext context;
+
+  @Autowired
+  private EfgsProperties properties;
+
+  @Autowired
+  private DiagnosisKeyEntityRepository diagnosisKeyEntityRepository;
+
+  @Autowired
+  private CertificateAuthentificationFilter certFilter;
+
+  @Autowired
+  private CertificateRepository certificateRepository;
+
+  private SignatureGenerator signatureGenerator;
+
+  private MockMvc mockMvc;
+
+  @Before
+  public void setup() throws NoSuchAlgorithmException, CertificateException, CertIOException,
+    OperatorCreationException {
+    TestData.insertCertificatesForAuthentication(certificateRepository);
+
+    diagnosisKeyEntityRepository.deleteAll();
+    mockMvc = MockMvcBuilders
+      .webAppContextSetup(context)
+      .addFilter(certFilter)
+      .build();
+  }
+
+  @Test
+  public void testInvalidTransmissionRiskLevel() throws Exception {
+    EfgsProto.DiagnosisKey diagnosisKey =
+      TestData.getDiagnosisKeyProto().toBuilder().setTransmissionRiskLevel(100).build();
+
+    EfgsProto.DiagnosisKeyBatch batch = EfgsProto.DiagnosisKeyBatch.newBuilder()
+      .addAllKeys(Arrays.asList(diagnosisKey)).build();
+
+    mockMvc.perform(post("/diagnosiskeys/upload")
+      .contentType("application/protobuf; version=1.0")
+      .header("batchTag", TestData.FIRST_BATCHTAG)
+      .header("batchSignature", "signature")
+      .header(properties.getCertAuth().getHeaderFields().getThumbprint(), TestData.AUTH_CERT_HASH)
+      .header(properties.getCertAuth().getHeaderFields().getDistinguishedName(), TestData.DN_STRING_DE)
+      .content(batch.toByteArray())
+    )
+      .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testInvalidReportType() throws Exception {
+    EfgsProto.DiagnosisKey diagnosisKey =
+      TestData.getDiagnosisKeyProto().toBuilder().setReportType(EfgsProto.ReportType.UNKNOWN).build();
+
+    EfgsProto.DiagnosisKeyBatch batch = EfgsProto.DiagnosisKeyBatch.newBuilder()
+      .addAllKeys(Arrays.asList(diagnosisKey)).build();
+
+    mockMvc.perform(post("/diagnosiskeys/upload")
+      .contentType("application/protobuf; version=1.0")
+      .header("batchTag", TestData.FIRST_BATCHTAG)
+      .header("batchSignature", "signature")
+      .header(properties.getCertAuth().getHeaderFields().getThumbprint(), TestData.AUTH_CERT_HASH)
+      .header(properties.getCertAuth().getHeaderFields().getDistinguishedName(), TestData.DN_STRING_DE)
+      .content(batch.toByteArray())
+    )
+      .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testInvalidKeyData() throws Exception {
+    EfgsProto.DiagnosisKey diagnosisKey =
+      TestData.getDiagnosisKeyProto().toBuilder().setKeyData(ByteString.EMPTY).build();
+
+    EfgsProto.DiagnosisKeyBatch batch = EfgsProto.DiagnosisKeyBatch.newBuilder()
+      .addAllKeys(Arrays.asList(diagnosisKey)).build();
+
+    mockMvc.perform(post("/diagnosiskeys/upload")
+      .contentType("application/protobuf; version=1.0")
+      .header("batchTag", TestData.FIRST_BATCHTAG)
+      .header("batchSignature", "signature")
+      .header(properties.getCertAuth().getHeaderFields().getThumbprint(), TestData.AUTH_CERT_HASH)
+      .header(properties.getCertAuth().getHeaderFields().getDistinguishedName(), TestData.DN_STRING_DE)
+      .content(batch.toByteArray())
+    )
+      .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testInvalidRollingStartIntervalNumber() throws Exception {
+    EfgsProto.DiagnosisKey diagnosisKey =
+      TestData.getDiagnosisKeyProto().toBuilder().setRollingStartIntervalNumber(0).build();
+
+    EfgsProto.DiagnosisKeyBatch batch = EfgsProto.DiagnosisKeyBatch.newBuilder()
+      .addAllKeys(Arrays.asList(diagnosisKey)).build();
+
+    mockMvc.perform(post("/diagnosiskeys/upload")
+      .contentType("application/protobuf; version=1.0")
+      .header("batchTag", TestData.FIRST_BATCHTAG)
+      .header("batchSignature", "signature")
+      .header(properties.getCertAuth().getHeaderFields().getThumbprint(), TestData.AUTH_CERT_HASH)
+      .header(properties.getCertAuth().getHeaderFields().getDistinguishedName(), TestData.DN_STRING_DE)
+      .content(batch.toByteArray())
+    )
+      .andExpect(status().isBadRequest());
+  }
+
+  @Test
+  public void testInvalidRollingPeriod() throws Exception {
+    EfgsProto.DiagnosisKey diagnosisKey =
+      TestData.getDiagnosisKeyProto().toBuilder().setRollingPeriod(0).build();
+
+    EfgsProto.DiagnosisKeyBatch batch = EfgsProto.DiagnosisKeyBatch.newBuilder()
+      .addAllKeys(Arrays.asList(diagnosisKey)).build();
+
+    mockMvc.perform(post("/diagnosiskeys/upload")
+      .contentType("application/protobuf; version=1.0")
+      .header("batchTag", TestData.FIRST_BATCHTAG)
+      .header("batchSignature", "signature")
+      .header(properties.getCertAuth().getHeaderFields().getThumbprint(), TestData.AUTH_CERT_HASH)
+      .header(properties.getCertAuth().getHeaderFields().getDistinguishedName(), TestData.DN_STRING_DE)
+      .content(batch.toByteArray())
+    )
+      .andExpect(status().isBadRequest());
+  }
+
+}
