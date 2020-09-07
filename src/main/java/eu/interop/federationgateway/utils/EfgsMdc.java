bf128d8bb603c7006ec951f0b391d8fb93e68187
Felix Dittrich <felix.dittrich@t-systems.com>
2020-09-07 11:51:25 +0200

    ISO DateTime Format for logging

--- src/main/java/eu/interop/federationgateway/utils/EfgsMdc.java
+++ src/main/java/eu/interop/federationgateway/utils/EfgsMdc.java
@@ -1,5 +1,9 @@
 package eu.interop.federationgateway.utils;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import org.slf4j.MDC;
 
 /**
@@ -35,11 +39,30 @@ public class EfgsMdc {
    * <p>This method delegates all work to the MDC of the underlying logging system.
    *
    * @param key   non-null key
-   * @param value value to put in the map
+   * @param value a numeric value to put in the map
    * @throws IllegalArgumentException in case the "key" parameter is null
    */
   public static void put(String key, long value) {
     put(key, String.valueOf(value));
+  }
+
+  /**
+   * Put a diagnostic context value (the <code>value</code> parameter) as identified with the
+   * <code>key</code> parameter into the current thread's diagnostic context map. The
+   * <code>key</code> parameter cannot be null. The <code>value</code> parameter
+   * can be null only if the underlying implementation supports it.
+   *
+   * <p>This method delegates all work to the MDC of the underlying logging system.
+   *
+   * @param key   non-null key
+   * @param value a date value to put in the map
+   * @throws IllegalArgumentException in case the "key" parameter is null
+   */
+  public static void put(String key, Date value) {
+    put(
+      key,
+      DateTimeFormatter.ISO_DATE_TIME.format(ZonedDateTime.ofInstant(value.toInstant(), ZoneOffset.UTC))
+    );
   }
 
   /**
