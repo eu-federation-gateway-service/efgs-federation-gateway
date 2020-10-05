af237aa31f22d9892be55c114c7f7a0feff6165c
Felix Dittrich <31076102+f11h@users.noreply.github.com>
2020-10-05 14:52:07 +0200

    Doc: Added documentation for Docker image & Added scripts for certificate creation (#182)
    * Fixed  java.io.IOException: Stream Closed in keystore
    
    * Fixed docker-compose.yml
    Added documentation about creating certificates to README.md
    Added scripts for certificate creation
    
    Co-authored-by: Arturs Braucs <arturs.braucs@gmail.com>
    Co-authored-by: alstiefel <64828242+alstiefel@users.noreply.github.com>

---
+++ tools/create_callback_client_certificate.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+keystorefilename="efgs-cb-client.jks"
+keystorepassword="3fgs-p4ssw0rd"
+certCN="EFGS-Callback DEV"
+certC="DE"              
+yn=N
+
+ECHO [1 of 6] Deleting old files...
+rm callback.pem
+rm callback.key
+rm callback.p12
+rm ${keystorefilename}
+echo ... old files deleted.
+read -p "keystorefilename [${keystorefilename}]:" input
+keystorefilename=${input:-${keystorefilename}}
+read -p "keystorepassword [${keystorepassword}]: " input
+keystorepassword=${input:-${keystorepassword}}
+read -p "certCN [${certCN}]: " input
+certCN=${input:-${certCN}}
+read -p "certC [${certC}]: " input
+certC=${input:-${certC}}
+#while true; do
+#read -p "include public key in generated cert [${yn}]: " input
+#yn=${input:-$yn}
+# case $yn in
+#        [Yy]* ) pk=-pubkey; break;;
+#        [Nn]* ) break;;
+#        * ) echo "Please answer Y(es) or N(o).";;
+# esac
+#done
+echo [2 of 6] Creating certificate...
+openssl req -nodes -new -x509 -keyout callback.key -out callback.pem -days 720 -subj "//C=${certC}\CN=${certCN}\O=EFGS DEV Org"
+echo ... Certificate created.
+
+echo [3 of 6] Creating keystore...
+keytool -genkey -keyalg RSA -keystore ${keystorefilename} -storepass ${keystorepassword} -keysize 2048 -alias dummykey -dname C=Country -validity 1 -keypass ${keystorepassword}
+echo ... keystore created.
+
+echo [4 of 6] Combine certificate and private key, output to PKCS#12 file...
+openssl pkcs12 -export -in callback.pem -inkey callback.key -out callback.p12 -password pass:${keystorepassword} -name efgs_callback_key
+echo ... combined certificate created.
+
+echo [5 of 6] Adding certificates to keystore...
+keytool -importkeystore -deststorepass ${keystorepassword} -destkeypass ${keystorepassword} -destkeystore ${keystorefilename} -srckeystore callback.p12 -srcstoretype PKCS12 -srcstorepass ${keystorepassword} -alias efgs_callback_key -noprompt
+keytool -importcert -alias efgs_callback_cert -file callback.pem -keystore ${keystorefilename} -storepass ${keystorepassword} -noprompt
+echo ... certificates added.
+
+echo [6 of 6] Deleting dummy cert from keystore...
+keytool -delete -keystore ${keystorefilename} -storepass ${keystorepassword} -alias dummykey
+echo ... deleted dummy cert from keystore.
