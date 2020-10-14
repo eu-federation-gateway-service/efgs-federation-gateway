a501cec2d872a6c00d90d51df918bc10bd2b7b7e
Felix Dittrich <felix.dittrich@t-systems.com>
2020-10-14 10:23:55 +0200

    Added padding for cert hash calculation

--- src/main/java/eu/interop/federationgateway/filter/CertificateAuthentificationFilter.java
+++ src/main/java/eu/interop/federationgateway/filter/CertificateAuthentificationFilter.java
@@ -102,7 +102,13 @@ public class CertificateAuthentificationFilter extends OncePerRequestFilter {
       return inputString;
     } else {
       try {
-        return new BigInteger(1, Base64.getDecoder().decode(inputString)).toString(16);
+        String hexString = new BigInteger(1, Base64.getDecoder().decode(inputString)).toString(16);
+
+        if (hexString.length() == 63) {
+          hexString = "0" + hexString;
+        }
+
+        return hexString;
       } catch (IllegalArgumentException ignored) {
         log.error("Could not normalize certificate hash.");
         return null;
